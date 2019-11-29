fe27e6516a6ed346ced762ec7ea73f2855412d85
kais42 <49623504+kais42@users.noreply.github.com>
2019-11-29 09:04:56 +0000

    Add files via upload

---
+++ projet1/connexion.h
@@ -0,0 +1,20 @@
+#ifndef CONNEXION_H
+#define CONNEXION_H
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+
+
+class Connexion
+{
+private:
+    QSqlDatabase db;
+public:
+    Connexion();
+    bool ouvrirConnexion();
+    void fermerConnexion();
+
+};
+
+#endif // CONNEXION_H
